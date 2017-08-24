15d415a0ee1c4f23456d1747441c10198d2129ac
pvanphuc <vphuc.p@gmail.com>
2017-08-25 05:08:56 +0700

    Initial commit

---
+++ math-question/app.js
@@ -0,0 +1,81 @@
+var App;
+var app = function(){
+	/*Get Element*/
+	var eLoader = document.getElementById('loader');
+	var eContent = document.getElementById('container');
+	var eQuestion = document.getElementById('question');
+	var eSolution = document.getElementById('solution');
+	var eTip = document.getElementById('tip');
+	var eNumber = document.getElementById('number');
+
+
+	/*Loader*/
+	this.setDisplayLoader = function(pDisplay){
+
+		eLoader.style.display = pDisplay;
+	}
+	this.setDisplayContent = function(pDisplay){
+
+		eContent.style.display = pDisplay;
+	}
+	this.setText= function(i){
+		eQuestion.innerHTML = Data[i].question;
+		eSolution.innerHTML = Data[i].solution;
+		eTip.innerHTML = Data[i].tip;
+		eNumber.innerHTML = i;
+
+	}
+
+
+	/*Content*/
+
+};
+var index = 0
+var maxId = Data.length-1;
+var plusSlides;
+var onloadD = function (){
+	App = new app();
+	App.setDisplayLoader('none');
+	App.setDisplayContent('block');
+	plusSlides = function(p){
+		index = index + p;
+
+		if(index<0){
+			index = maxId;
+		}
+		if(index>maxId){
+			index = 0;
+		}
+		console.log(index);
+		App.setText(index);
+		MathJax.Hub.Typeset()
+
+	}
+	App.setText(index);
+}
+
+/*
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
+};
+
+getJSON('data/version.json',
+function(err, data) {
+  if (err != null) {
+    alert('Something went wrong: ' + err);
+  } else {
+    console.log('Your query count: ' + data);
+  }
+});
+*/
---
+++ math-question/data/data1.js
@@ -0,0 +1,57 @@
+var Data = [
+{
+	question: `Cho \\( (C) \\) \\(y=\\frac{x+2}{x-1} \\)
+	 v&agrave; \\( A(0,a) \\). T&igrave;m \\( a\\epsilon R \\) &#273;&#7875; t&#7915; \\( A \\) c&oacute;
+	 hai ti&#7871;p tuy&#7871;n &#273;&#7871;n &#273;&#7891; th&#7883; \\( (C) \\).<br />
+	A. \\(-2< a < -1 \\) <br />
+	B. \\(1 < a < 2 \\) <br />
+	C. \\(\\frac{-2}{3} < a < -1 \\) <br />
+	D. \\( \\frac{2}{3} < a < 1\\)`,
+	solution: `Kh&ocirc;ng c&oacute; c&aacute;i &#273;&aacute;p &aacute;n n&agrave;o gi&#7889;ng c&#7843;.<br />
+	Sai &#273;&#7873;, t&iacute;nh sai hay l&#7847;m ch&#7895; n&agrave;o...nh&#432;ng d&#7841;ng n&agrave;y l&agrave; gi&#7843;i ki&#7871;u n&agrave;y &#273;&oacute;.`,
+	tip: `Ta c&oacute; &#273;&#7841;o h&agrave;m \\( ( C ) \\) l&agrave; \\( y'=\\frac{-3}{(x-1)^{2}} \\).<br/>
+	&#272;&#432;&#7901;ng th&#7859;ng qua \\( A \\) l&agrave;: \\( y = kx + a \\).<br/>
+	X&eacute;t t&#7841;i ti&#7871;p &#273;i&#7875;m ti&#7871;p tuy&#7871;n n&#7871;u c&oacute; th&igrave;.(V&igrave; y c&#7911;a &#273;&#7891; th&#7883; (C) v&agrave; y c&#7911;a &#273;&#432;&#7901;ng th&#7859;ng ph&#7843;i gi&#7889;ng nhau). N&ecirc;n:
+	$$ y=kx+a=\\frac{x+2}{x-1} $$
+	T&#7841;i ti&#7871;p &#273;i&#7875;m \\( k \\) ch&iacute;nh l&agrave; &#273;&#7897; d&#7889;c c&#7911;a ti&#7871;p tuy&#7871;n n&oacute; s&#7869; b&#7857;ng &#273;&#7841;o h&agrave;m \\( y' \\) c&#7911;a &#273;&#7891; th&#7883; \\( (C) \\). Ta c&oacute;:<br />
+	X&eacute;t \\( k=0 \\) kh&ocirc;ng th&#7887;a m&atilde;n v&igrave; \\( y' \\neq 0 \\).<br />
+	V&#7871; ph&#7843;i tr&#7915; \\( 2 \\) r&#7891;i chia cho \\( x \\) ta &#273;&#432;&#7907;c \\( k \\) l&agrave;:
+	$$ k=\\frac{x(1-a)+(2+a)}{x(x-1)} $$.
+	V&#7899;i \\( k = y' \\)::
+	$$  y'= k =\\frac{x(1-a)+(2+a)}{x(x-1)} =\\frac{-3}{(x-1)^{2}} $$ 
+	&#272;&#7875; c&oacute; hai ti&#7871;p tuy&#7871;n th&igrave; ph&#432;&#417;ng tr&igrave;nh tr&ecirc;n ph&#7843;i c&oacute; hai nghi&#7879;m \\( x \\) v&#7899;i \\( x \\neq 0 ; x\\neq 1 \\).<br/>
+	V&#7899;i \\( x \\neq 0 ; x \\neq 1 \\) quy &#273;&#7891;ng r&#7891;i x&oacute;a m&#7851;u ta &#273;&#432;&#7907;c ph&#432;&#417;ng tr&igrave;nh:
+	$$ (x(1-a)+(2+a))(x-1)=-3x  $$
+	$$ \\Leftrightarrow x^2(1-a)+2x(a+2)-(a+2)=0 $$
+	Ph&#432;&#417;ng tr&igrave;nh b&#7853;c hai n&ecirc;n t&#7889;i &#273;a l&agrave; 2 nghi&#7879;m.<br/>
+	Thay \\( x=0 \\) ta c&oacute; \\( a = -2 \\). Thay \\( x=1 \\) ta c&oacute; \\( 3=0 \\) v&ocirc; l&yacute;. V&igrave; \\( x \\neq 0 \\) n&ecirc;n:<br />
+	\\( a \\neq -2  \\). &#272;&#7863;t l&agrave; \\( (1) \\). <br/>
+	\\( \\Delta \\) c&#7911;a ph&#432;&#417;ng tr&igrave;nh tr&ecirc;n l&agrave;:<br />
+	$$  4(a+2)^2-4(a+2)(1-a) $$
+	Hai nghi&#7879;m th&igrave; \\( \\Delta > 0 \\) t&#432;&#417;ng &#273;&#432;&#417;ng:
+	$$ (a+2)(2a+1)>0 $$
+	Gi&#7843;i &#273;&#417;n gi&#7843;n &#273;&#432;&#7907;c n&oacute; n&#7857;m ngo&agrave;i kho&#7843;ng hai nghi&#7879;m.<br />
+	\\(a<-2 ||a>\\frac{-1}{2} \\). &#272;&#7863;t l&agrave; \\( (2) \\).<br/>
+	T&#7915; \\( (1) \\) v&agrave; \\( (2) \\) ta c&oacute; k&#7871;t qu&#7843;:
+	$$ a<-2  ||  a>\\frac{-1}{2} $$
+	D&#7845;u \\( || \\) l&agrave; ho&#7863;c &#273;&oacute;.
+	`
+},
+{
+	question: `Tu&#7845;n x&oacute;a 1 trong 10 s&#7889; d&#432;&#417;ng li&ecirc;n ti&#7871;p, t&#7893;ng 9 s&#7889; c&ograve;n l&#7841;i l&agrave; 2006. H&#7887;i Tu&#7845;n x&oacute;a s&#7889; n&agrave;o?<br />
+	A. 218<br />
+	B. 219<br />
+	C. 220<br />
+	D. 225<br />
+	E. 227`,
+	solution1: 'B. 219',
+	tip1: 'T&#7893;ng m&#432;&#7901;i s&#7889; th&igrave; ph&#7843;i chia h&#7871;t cho 5 (c&oacute; 5 c&#7863;p b&#7857;ng gi&aacute; tr&#7883; l&#7899;n nh&#7845;t c&#7897;ng b&eacute; nh&#7845;t). V&#7853;y ch&#7881; c&oacute; &#273;&aacute;p &aacute;n B. Ch&#432;a th&#7917; l&#7841;i n&ecirc;n th&#7917; l&#7841;i &#273;&#7875; xem m&#432;&#7901;i s&#7889; l&agrave; s&#7889; n&agrave;o &#273;&ocirc;i khi m&#7845;y th&#7857;ng ra &#273;&#7873; sai. C&ograve;n khi thi th&igrave; &#273;&#7915;ng th&#7917; l&#7841;i t&#7889;n th&#7901;i gian.'
+},
+
+
+
+]
+
+
+
+
